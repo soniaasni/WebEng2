ffdf97bb5109c1757d664a37bd66903ec19cc978
soniaasni <111530502+soniaasni@users.noreply.github.com>
2026-04-30 13:02:10 +0200

    feat: register service worker (#10)
    Closes #10

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+const CACHE_NAME = 'webeng2-v1';
+
+self.addEventListener('install', () => {
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(clients.claim());
+});
