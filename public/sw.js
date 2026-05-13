96ea5e48fa75916e0a3fdc54f975da5e650d2430
soniaasni <111530502+soniaasni@users.noreply.github.com>
2026-05-13 09:16:04 +0200

    fix: Service Worker mit App-Shell-Caching und Cache-Cleanup vervollständigen
    - install: APP_SHELL-Assets in Cache laden (index.html, manifest.json, Icons)
    - activate: veraltete Cache-Versionen löschen vor clients.claim()
    - CACHE_NAME wird jetzt tatsächlich verwendet

--- public/sw.js
+++ public/sw.js
@@ -1,9 +1,26 @@
 const CACHE_NAME = 'webeng2-v1';
 
-self.addEventListener('install', () => {
+const APP_SHELL = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/img/Icon.png',
+  '/img/Icon.jpeg',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL))
+  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
+  event.waitUntil(
+    caches.keys()
+      .then((keys) => Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      ))
+      .then(() => clients.claim())
+  );
 });
